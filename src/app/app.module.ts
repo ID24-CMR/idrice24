6c891587d0ca13ca7cd075cd740c0bfb7b4e406e
ID24-CMR <idrice.tsafouet@yahoo.com>
2024-10-21 07:19:21 +0200

    designing the header #22 #21

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,4 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
-import { SharedModule } from './shared/shared.module';
-import { CoreModule } from './core/core.module';
-import { AppComponent } from './app.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 
 import { HeaderComponent } from './shared/components/header/header.component';
@@ -12,7 +6,6 @@ import { HeaderComponent } from './shared/components/header/header.component';
 
 
 @NgModule({
-  declarations: [ HeaderComponent ],
   imports: [ ],
   providers: [],
   bootstrap: [HeaderComponent]
