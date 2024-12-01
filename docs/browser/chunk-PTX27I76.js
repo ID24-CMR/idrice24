6f0830799a44bc818777a08e68b62f03517d27db
ID24-CMR <idrice.tsafouet@yahoo.com>
2024-12-01 05:58:25 +0100

    update doc folder #18

--- docs/browser/chunk-PTX27I76.js
+++
@@ -1,1 +0,0 @@
-import{Ga as r,K as t,S as e}from"./chunk-WMJ6EZG4.js";var d=(()=>{class o{static \u0275fac=function(i){return new(i||o)};static \u0275mod=e({type:o});static \u0275inj=t({imports:[r]})}return o})();export{d as ProductModule};
